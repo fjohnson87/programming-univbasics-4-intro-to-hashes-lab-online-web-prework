a26e70ffa6cb00eba5dc3baa564d7e0eb0accb39
legacy-gopher-9954 <fabianjohnson07@gmail.com>
2019-11-04 22:44:19 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,25 +1,32 @@
 def new_hash
   # return an empty hash
+  Magazine = {name: “”}
 end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
+  my_hash = {“name: “Antiques”,
+Series: 31}
 end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  Pioneer = {:name = >“Grace Hopper”}
 end
 
 def id_generator
   # return a hash with a key :id assigned to positive integer
+  id_generator => {:id => 2}
 end
 
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
+  ({:book =>foreign, foreign = marble} )
 end
 
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
+  read_from_hash{:book =>foreign, table => marble} 
 end
 
 def update_counting_hash(hash, key)
