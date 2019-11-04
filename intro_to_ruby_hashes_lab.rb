db3b23a0e9fd7859c6a6d28e1b8d3754ae2b372c
legacy-gopher-9954 <fabianjohnson07@gmail.com>
2019-11-04 22:45:59 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -33,4 +33,9 @@ def update_counting_hash(hash, key)
   # given a hash an a key as parameters, return an updated hash
   # if the provided key is not present in the hash, add it and assign it to the value of 1
   # if the provided key is present, increment its value by 1
+  guide = {:book “foreign”, :play 1} 
+guide [:play]
+
+guide [:play] =+1
+
 end
